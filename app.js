0dd4f2407b11a784aa17ed7fa25488df6ae25bf8
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-01-27 14:53:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express")
 const mysql=require("mysql")
 
 var db=mysql.createConnection({
-    host     : 'b74l2ilqg8dyheq5tdny-mysql.services.clever-cloud.com',
-    user     : 'u7pwz0zs80fl7zyr',
-    password : 'XIYUjmu9X3shOw1fpcBH',
-    database : 'b74l2ilqg8dyheq5tdny'
+    host     : '-mysql.services.clever-cloud.com',
+    user     : '',
+    password : '',
+    database : ''
 })
 
 db.connect((err)=>{
